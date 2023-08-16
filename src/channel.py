88bbc2e44228a5c60ef49986d3add2beabb351da
Dmitri Camenev <dmit.cameneff@yandex.ru>
2023-08-16 08:39:06 +0300

    add static metod to class Channel

--- src/channel.py
+++ src/channel.py
@@ -4,23 +4,12 @@ from dotenv import load_dotenv
 from googleapiclient.discovery import build
 
 
-def load_credentials():
-    """Загрузка токена YouTube в переменную среды"""
-    env_file = '..\\src\\app.env'
-    load_dotenv(env_file)
-
-
-def printj(dict_to_print: dict) -> None:
-    """Выводит словарь в json-подобном удобном формате с отступами"""
-    print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
-
-
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        load_credentials()
+        Channel.load_credentials()
         api_key: str = os.getenv('YT_KEY')
         self.youtube = build('youtube', 'v3', developerKey=api_key)
         self.channel_id = channel_id
@@ -28,7 +17,18 @@ class Channel:
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        printj(channel)
+        Channel.printj(channel)
+
+    @staticmethod
+    def printj(dict_to_print: dict) -> None:
+        """Выводит словарь в json-подобном удобном формате с отступами"""
+        print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+
+    @staticmethod
+    def load_credentials():
+        """Загрузка токена YouTube в переменную среды"""
+        env_file = '..\\src\\app.env'
+        load_dotenv(env_file)
 
 
 if __name__ == '__main__':
