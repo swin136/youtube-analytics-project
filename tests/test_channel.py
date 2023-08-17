f0d0e52d6ce151b5af071691deb6a980512f5fe0
Dmitri Camenev <dmit.cameneff@yandex.ru>
2023-08-17 11:58:10 +0300

    fix PEP8 bugs

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -1,5 +1,7 @@
 import pytest
 from src.channel import Channel
+
+
 @pytest.fixture()
 def get_channels_to_test():
     """Фикстура с тестовыми channel_id каналов YouTube"""
@@ -8,13 +10,14 @@ def get_channels_to_test():
         'Мир Православия': 'UCmSIOnaJ7oLugaqOqI2GnCg',
     }
 
+
 def test_channel_init(get_channels_to_test):
     """Тестирование инициализации объекта YouTube и получения сведений о канале"""
     # Тестируем получение информации о канале "MoscowPython"
-    mychannel = Channel(get_channels_to_test['MoscowPython'])
-    assert mychannel.title == 'MoscowPython'
-    assert mychannel.custom_url == 'https://www.youtube.com/@moscowdjangoru'
+    my_channel = Channel(get_channels_to_test['MoscowPython'])
+    assert my_channel.title == 'MoscowPython'
+    assert my_channel.custom_url == 'https://www.youtube.com/@moscowdjangoru'
     # Тестируем получение информации о канале "Мир Православия"
-    mychannel = Channel(get_channels_to_test['Мир Православия'])
-    assert mychannel.title == 'Мир Православия'
-    assert mychannel.custom_url == 'https://www.youtube.com/@user-mirpravoslavia'
+    my_channel = Channel(get_channels_to_test['Мир Православия'])
+    assert my_channel.title == 'Мир Православия'
+    assert my_channel.custom_url == 'https://www.youtube.com/@user-mirpravoslavia'
