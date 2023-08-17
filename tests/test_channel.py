2119136268ba18f8282d53eec458476f2a95283d
Dmitri Camenev <dmit.cameneff@yandex.ru>
2023-08-17 19:18:14 +0300

    add test for magic methods of class Channel

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -6,19 +6,19 @@ from src.channel import Channel
 def get_channels_to_test():
     """Фикстура с тестовыми channel_id каналов YouTube"""
     return {
-        'MoscowPython': "UC-OVMPlMA3-YCIeg4z5z23A",
-        'Мир Православия': 'UCmSIOnaJ7oLugaqOqI2GnCg',
+        'MoscowPython': Channel("UC-OVMPlMA3-YCIeg4z5z23A"),
+        'Мир Православия': Channel('UCmSIOnaJ7oLugaqOqI2GnCg'),
     }
 
 
 def test_channel_init(get_channels_to_test):
     """Тестирование инициализации объекта YouTube и получения сведений о канале"""
     # Тестируем получение информации о канале "MoscowPython"
-    my_channel = Channel(get_channels_to_test['MoscowPython'])
+    my_channel = get_channels_to_test['MoscowPython']
     assert my_channel.title == 'MoscowPython'
     assert my_channel.custom_url == 'https://www.youtube.com/@moscowdjangoru'
     # Тестируем получение информации о канале "Мир Православия"
-    my_channel = Channel(get_channels_to_test['Мир Православия'])
+    my_channel = get_channels_to_test['Мир Православия']
     assert my_channel.title == 'Мир Православия'
     assert my_channel.custom_url == 'https://www.youtube.com/@user-mirpravoslavia'
 
@@ -26,5 +26,32 @@ def test_channel_init(get_channels_to_test):
 def test_access_attribute(get_channels_to_test):
     """Тестируем механизм защиты атрибутов объекта"""
     with pytest.raises(AttributeError):
-        my_channel = Channel(get_channels_to_test['MoscowPython'])
+        my_channel = get_channels_to_test['MoscowPython']
         my_channel.channel_id = "Тестовый_канал_id"
+
+
+def test_repr_method(get_channels_to_test):
+    """Тестируем метод строкового представления объекта класса"""
+    my_channel = get_channels_to_test['MoscowPython']
+    assert str(my_channel) == 'MoscowPython (https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A)'
+
+    my_channel = get_channels_to_test['Мир Православия']
+    assert str(my_channel) == 'Мир Православия (https://www.youtube.com/channel/UCmSIOnaJ7oLugaqOqI2GnCg)'
+
+
+def test_add_method(get_channels_to_test):
+    """Тестируем магический метод __add__ для сложения подписчиков каналов YouTube"""
+    channel_1 = get_channels_to_test['MoscowPython']
+    channel_2 = get_channels_to_test['Мир Православия']
+    result_sum = channel_1.subscriber_count + channel_2.subscriber_count
+
+    assert channel_1 + channel_2 == result_sum
+
+def test_sub_method(get_channels_to_test):
+    """Тестируем магический метод __sub__ для разности подписчиков каналов YouTube"""
+    channel_1 = get_channels_to_test['MoscowPython']
+    channel_2 = get_channels_to_test['Мир Православия']
+    result_sub = channel_1.subscriber_count - channel_2.subscriber_count
+
+    assert channel_1 - channel_2 == result_sub
+
