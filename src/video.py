b6e3b5e01106d9dedbfef340f004f3bed18a872a
Dmitri Camenev <dmit.cameneff@yandex.ru>
2023-08-24 08:00:37 +0300

    fix bugs

--- src/video.py
+++ src/video.py
@@ -1,35 +1,37 @@
-import os
 from src.channel import Channel
-from googleapiclient.discovery import build
 from pprint import pprint
 
 
 class Video:
     """Класс для ролика YouTube"""
-
+    __youtube_obj = None
     def __init__(self, video_id: str) -> None:
-        Channel.load_credentials()
-        api_key: str = os.getenv('YT_KEY')
-        self.__youtube_object = build('youtube', 'v3', developerKey=api_key)
-
+        # Создает объект для доступа к YouTube API
+        if Video.__youtube_obj is None:
+            Video.__youtube_obj = Channel.create_youtube_object()
         # Заполняем аттрибуты экземпляра класса
         self.__video_id = video_id
 
-        video_response = self.__youtube_object.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                             id=self.video_id
-                                                             ).execute()
+        video_response = Video.__youtube_obj.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                              id=self.video_id
+                                                              ).execute()
 
-        # название видео
-        self.__title = video_response['items'][0]['snippet']['title']
+        try:
+            # название видео
+            self.__title = video_response['items'][0]['snippet']['title']
 
-        # количество просмотров
-        self.__viewCount = int(video_response['items'][0]['statistics']['viewCount'])
-        # количество лайков
-        self.__likeCount = int(video_response['items'][0]['statistics']['likeCount'])
+            # количество просмотров
+            self.__viewCount = int(video_response['items'][0]['statistics']['viewCount'])
+            # количество лайков
+            self.__likeCount = int(video_response['items'][0]['statistics']['likeCount'])
+        except (IndexError, KeyError):
+            self.__title = None
+            self.__viewCount = None
+            self.__likeCount = None
 
     def print_video_info(self):
-        video_response = self.__youtube_object.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                             id=self.video_id).execute()
+        video_response = Video.__youtube_obj.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                              id=self.video_id).execute()
         pprint(video_response)
 
     def __str__(self):
@@ -48,7 +50,7 @@ class Video:
 
     @property
     def youtube_object(self):
-        return self.__youtube_object
+        return Video.__youtube_obj
 
     @property
     def title(self):
@@ -64,7 +66,8 @@ class Video:
 
     @property
     def link_video(self):
-        return f'https://www.youtube.com/watch?v={self.video_id}'
+        # return f'https://www.youtube.com/watch?v={self.video_id}'
+        return f'https://youtu.be/{self.video_id}'
 
 
 class PLVideo(Video):
@@ -77,5 +80,4 @@ class PLVideo(Video):
         return self.__playlist_id
 
     def __repr__(self):
-        result = super().__repr__()
-        return result + '\n' + f"id плейлиста: {self.playlist_id}"
+        return super().__repr__() + '\n' + f"id плейлиста: {self.playlist_id}"
